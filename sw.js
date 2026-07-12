31544dd6a427ba788c5eadd134f966f404133e8d
isaacoolaio-arch <isaac.oola.io@gmail.com>
2026-07-12 19:35:54 +0300

    v14: multi-select video import for folder-based training library workflow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'toolfix-v13';
+const CACHE = 'toolfix-v14';
 const ASSETS = [
   './', './index.html', './data.js', './manifest.json',
   './img/drill.webp', './img/drill-plate.webp',
